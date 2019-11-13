f6d6cac62cee56fe1822312c7eb170e13be77c3f
Clairton Carneiro Luz <clairton.luz@seduc.ce.gov.br>
2019-11-13 11:57:12 -0300

    configurando projeto

--- src/app.test.ts
+++
@@ -1,7 +0,0 @@
-import req from "supertest";
-import app from "./app";
-
-test("[GET] /", async () => {
-  const res = await req(app).get("/");
-  expect(res.text).toBe("Hello ts-node!");
-});
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import app from './app';
-
-app.listen(3000, () => {
+import server from './server';
+server.listen(3000, () => {
   console.log(`[SERVER] Running at http://localhost:3000`);
 });
